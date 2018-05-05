22b54056c05be0ef626864f76d340b273b8ce65a
Bermudez Rebolledo <nancy.bermudez@capgemini.com>
2018-05-05 16:51:35 -0500

    Adding Unit Test.

--- src/test/java/com/opensource/app/persistence/MongoDbTest.java
+++ src/test/java/com/opensource/app/persistence/MongoDbTest.java
@@ -14,6 +14,8 @@ import org.junit.Test;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.opensource.app.models.Weight;
@@ -21,21 +23,23 @@ import com.opensource.app.models.Weight;
 public class MongoDbTest {
 	MongoClient mongoClient;
 	DB dataBase;
+	private static final String DATABASE_NAME="java-junit";
+	private static final String COLLECTION_NAME = "Weights";
 	
 	@Before
 	public void setupTest() throws UnknownHostException {
 		System.out.println("soy el metodo SetupTest");
 		mongoClient = new MongoClient();
-		dataBase = mongoClient.getDB("java-junit");
+		dataBase = mongoClient.getDB(DATABASE_NAME);
 	}
 	
 	@Test
 	public void writeIntoDb() {
-		System.out.println("soy el metodo que escribe en la base de datos");
+		
 		DBObject weight = new BasicDBObject("_id","123").append("value",63).append("unit", "kg");
 		DBObject result = new BasicDBObject();
-		dataBase.getCollection("Weights").insert(weight);
-		result = dataBase.getCollection("Weights").findOne();
+		dataBase.getCollection(COLLECTION_NAME).insert(weight);
+		result = dataBase.getCollection(COLLECTION_NAME).findOne();
 		assertNotNull(result);
 		assertEquals("123",result.get("_id").toString());
 		assertEquals("63",result.get("value").toString());
@@ -44,14 +48,41 @@ public class MongoDbTest {
 	
 	@Test
 	public void readFromDb() {
-		System.out.println("Soy el método que lee de la base de datos.");
+	
 		List<DBObject> objectList = new ArrayList<DBObject>();
 		DBObject weight = new BasicDBObject("_id","1").append("value", 80).append("unit", "kg");
 		DBObject weight2 = new BasicDBObject("_id","2").append("value", 45).append("unit", "lb");
+		DBObject query = new BasicDBObject("_id","2");
 		objectList.add(weight);
 		objectList.add(weight2);
-		dataBase.getCollection("Weights").insert(objectList);
+		DBCollection collection = dataBase.getCollection(COLLECTION_NAME);
+		collection.insert(objectList);
 		
+		
+		DBCursor cursor = collection.find(query);
+		List<DBObject> result = new ArrayList<DBObject>();
+		
+		while(cursor.hasNext()) {
+			result.add(cursor.next());		
+		}
+		
+		for(DBObject element: result) {
+			System.out.println(element.toString());
+			assertNotNull(element);
+			assertEquals("2", element.get("_id").toString());
+			assertEquals("lb",element.get("unit").toString());
+		}
+		cursor.close();
+	}
+	
+	@Test
+	public void deleteDocument() {
+		DBObject weight = new BasicDBObject("_id","1").append("value", 90).append("unit", "kg");
+		DBCollection collection = dataBase.getCollection(COLLECTION_NAME);
+		collection.insert(weight);
+		
+		DBObject documentToDelete = new BasicDBObject("_id","1");
+		collection.remove(documentToDelete);
 	}
 	
 	@After
